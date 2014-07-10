b0effa2ee353a66ab46f28211455cc710e9d766a
Greg Silin <greg.silin@nitropdf.com>
2014-07-09 18:25:06 -0700

    Change settings to finite duration & clean up the conf parsing

--- src/main/scala/redis/config/RedisConfig.scala
+++ src/main/scala/redis/config/RedisConfig.scala
@@ -9,22 +9,21 @@ object RedisConfig {
 
   private lazy val conf: Config = ConfigFactory.load()
 
-  lazy val RedisConnectionTimeout: FiniteDuration = {
-    val timeoutValue: Int = Try(conf getInt("rediscala.connection.timeout")) getOrElse {
-      println("Connection setting missing, falling back to default")
-      15
+  private def parseDuration(s: String): FiniteDuration = {
+    val duration = Duration(s)
+    duration match {
+      case t: FiniteDuration => t
+      case _ => throw new IllegalArgumentException(s"Cannot parse $s as a duration.")
     }
+  }
 
-    println(s"Redis connection timeout $timeoutValue seconds")
-    timeoutValue.seconds
+  lazy val RedisConnectionTimeout: FiniteDuration = {
+    val confValue = Option(conf getString "rediscala.connection.timeout")
+    confValue map parseDuration getOrElse 15.seconds
   }
 
   lazy val RedisReconnectTimeout: FiniteDuration = {
-    val timeoutValue: Int = Try(conf getInt("rediscala.reconnect.timeout")) getOrElse {
-      println("Reconnect setting missing, falling back to default")
-      2
-    }
-    println(s"Redis connection timeout $timeoutValue seconds")
-    timeoutValue.seconds
+    val confValue = Option(conf getString "rediscala.reconnect.interval")
+    confValue map parseDuration getOrElse 2.seconds
   }
 }
